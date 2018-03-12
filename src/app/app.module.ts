b5a22bfd61d38cf2cb98dc575c176c0b192a1d2e
Jason <jason.r.mcneilly@gmail.com>
2018-03-12 15:27:10 -0400

    added basic routing for loan and home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoanComponent } from './loan/loan.component';
 import { LoanService } from './loan/loan.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const routes: Routes = [
+  { path: 'loans', component: LoanComponent, },
+  { path: '', redirectTo: '/loans', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoanComponent
+    LoanComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    )
   ],
   providers: [
     LoanService
