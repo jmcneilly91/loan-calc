1192ae0446eb92c3210a3000d10174c9008fcb99
Jason <jason.r.mcneilly@gmail.com>
2018-03-19 17:04:37 -0400

    moved loan details to their own component

---
+++ src/app/loan-detail/loan-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'loan-detail',
+    templateUrl: './loan-detail.component.html',
+    styleUrls: [ './loan-detail.component.css' ]
+})
+export class LoanDetailComponent {}
